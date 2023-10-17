70bc9a837ebcba9e0fae81216b1bc323269b51be
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-17 23:28:19 +0900

    Add files via upload

---
+++ java/chap01/SystemOutPrintln.java
@@ -0,0 +1,12 @@
+class SystemOutPrintln{
+	public static void main(String args[]){
+		System.out.println("2 + 5 = " + 2 + 5);
+		System.out.println("2 + 5 = " + (2 + 5));
+
+		System.out.println(10+2);
+		System.out.println("1" + 2);
+		System.out.println("1"+"2");
+		System.out.println(1+"2");
+		System.out.println("12");
+	}
+}
