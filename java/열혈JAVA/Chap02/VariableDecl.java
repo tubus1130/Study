1aafddc110ba68175440734cd6dba6eb3284a041
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-10-18 22:48:50 +0900

    실수 자료형의 표현

---
+++ java/열혈JAVA/Chap02/VariableDecl.java
@@ -0,0 +1,11 @@
+public class VariableDecl {
+	public static void main(String[] args){
+		double num1, num2;
+		double result;
+		num1 = 1.01;
+		num2 = 2.01;
+		result = num1 + num2;
+		System.out.println(result);
+	}
+}
+// 3.0000001999999997
